bcf6ad338670cd28b250cd36a4f12efde3a382d2
Andrey O <ozzy.ext@gmail.com>
2022-05-20 03:58:14 +0300

    First implementation

--- src/MyLab.ProtocolStorage/Controllers/ProtocolsControllerV1.cs
+++ src/MyLab.ProtocolStorage/Controllers/ProtocolsControllerV1.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Mvc;
+using MyLab.Log.Dsl;
+using MyLab.ProtocolStorage.Models;
+using MyLab.RabbitClient.Publishing;
+using MyLab.Search.Searcher.Client;
+using QuerySearchStrategy = MyLab.Search.Searcher.Client.QuerySearchStrategy;
 
 namespace MyLab.ProtocolStorage.Controllers
 {
@@ -6,9 +12,63 @@ namespace MyLab.ProtocolStorage.Controllers
     [ApiController]
     public class ProtocolsControllerV1 : ControllerBase
     {
-        [HttpPost]
-        public IActionResult PushEntity()
+        private readonly IRabbitPublisher _rabbitPublisher;
+        private readonly ISearcherApiV3 _searcherApi;
+        private readonly IDslLogger _log;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ProtocolsControllerV1"/>
+        /// </summary>
+        public ProtocolsControllerV1(
+            IRabbitPublisher rabbitPublisher,
+            ISearcherApiV3 searcherApi,
+            ILogger<ProtocolsControllerV1> logger)
         {
+            _rabbitPublisher = rabbitPublisher;
+            _searcherApi = searcherApi;
+            _log = logger.Dsl();
+        }
+
+        [HttpPost("{protocolId}/collector")]
+        public IActionResult PushEntity([FromQuery] string protocolId, [FromBody] PushProtocolEntityRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(protocolId))
+                return BadRequest("ProtocolId is not specified");
+            
+            request.DateTime ??= DateTime.Now;
+
+            _rabbitPublisher.IntoDefault().SendJson(request).Publish();
+
+            return Ok();
+        }
+
+        [HttpPost("{protocolId}/searcher")]
+        public async Task<IActionResult> SearchAsync(
+            [FromQuery] string protocolId, 
+            [FromBody] ClientSearchRequest request, 
+            [FromHeader(Name = "X-Search-Token")] string searchToken)
+        {
+            if (string.IsNullOrWhiteSpace(protocolId))
+                return BadRequest("ProtocolId is not specified");
+
+            var searchReq = new ClientSearchRequestV3
+            {
+                Offset = request.Offset,
+                Limit = request.Limit,
+                Query = request.Query,
+                Filters = request.Filters?.Select(f =>
+                    new Search.Searcher.Client.FilterRef
+                    {
+                        Id = f.Id,
+                        Args = f.Args
+                    }
+                ).ToArray(),
+                QuerySearchStrategy = (QuerySearchStrategy)request.QuerySearchStrategy
+            };
+
+            var res = await _searcherApi.SearchAsync<JsonObject>(protocolId, searchReq, searchToken);
+
+            return Ok(res);
 
         }
     }
--- src/MyLab.ProtocolStorage/Program.cs
+++ src/MyLab.ProtocolStorage/Program.cs
@@ -1,6 +1,10 @@
 using MyLab.ApiClient;
+using MyLab.HttpMetrics;
+using MyLab.Log;
 using MyLab.RabbitClient;
 using MyLab.Search.Searcher.Client;
+using MyLab.WebErrors;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,20 +12,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 var srv = builder.Services;
 
-srv.AddControllers();
+srv.AddControllers(opts => opts.AddExceptionProcessing());
 
 srv.AddApiClients(reg =>
     {
         reg.RegisterContract<ISearcherApiV3>();
     }, builder.Configuration)
-    .AddRabbit(RabbitConnectionStrategy.Background);
+    .AddRabbit(RabbitConnectionStrategy.Background)
+    .AddRabbitTracing()
+    .AddLogging(l => l.AddMyLabConsole())
+    .AddUrlBasedHttpMetrics();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
+app.UseHttpMetrics();
 
 app.MapControllers();
+app.MapMetrics();
 
 app.Run();
